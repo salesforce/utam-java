0f7a2cbc3e501779363254a36c592e01e4a9d1d4
Elizaveta Ivanova <elizaveta.ivanova@salesforce.com>
2021-07-23 20:02:01 -0700

    JSON compiler config align with RFC

--- utam-compiler/src/test/java/utam/compiler/translator/JsonCompilerConfigTests.java
+++ utam-compiler/src/test/java/utam/compiler/translator/JsonCompilerConfigTests.java
@@ -9,17 +9,14 @@ package utam.compiler.translator;
 
 import static org.hamcrest.CoreMatchers.hasItems;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.anEmptyMap;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.hamcrest.Matchers.emptyIterable;
-import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.testng.Assert.expectThrows;
 import static utam.compiler.translator.JsonCompilerConfig.ERR_READING_COMPILER_CONFIG;
-import static utam.compiler.translator.JsonCompilerConfig.ERR_TARGET_NOT_SET;
 import static utam.compiler.translator.JsonCompilerConfig.Module.DEFAULT_JSON_FILE_MASK_REGEX;
 import static utam.compiler.translator.JsonCompilerConfig.Namespace.ERR_DUPLICATE_MAPPING;
 import static utam.compiler.translator.JsonCompilerConfig.Profile.ERR_DUPLICATE_PROFILE_DIFF_VALUES;
@@ -29,18 +26,18 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.List;
+import java.util.Map;
 import org.hamcrest.CoreMatchers;
 import org.testng.annotations.Test;
-import utam.compiler.UtamCompilationError;
 import utam.compiler.translator.JsonCompilerConfig.Module;
 import utam.compiler.translator.JsonCompilerConfig.Namespace;
 import utam.compiler.translator.JsonCompilerConfig.Profile;
-import utam.compiler.translator.JsonCompilerConfig.Target;
 import utam.core.declarative.translator.ProfileConfiguration;
 import utam.core.declarative.translator.TranslatorSourceConfig;
+import utam.core.declarative.translator.TranslatorTargetConfig;
 import utam.core.declarative.translator.UnitTestRunner;
 import utam.core.framework.consumer.UtamError;
-import utam.core.framework.context.StringValueProfile;
 
 /**
  * tests for json config
@@ -57,16 +54,47 @@ public class JsonCompilerConfigTests {
   }
 
   @Test
-  public void testValues() throws IOException {
+  public void testValuesFromJsonFile() throws IOException {
     JsonCompilerConfig config = getTestConfig();
-    ProfileConfiguration profileConfiguration = config.getConfiguredProfiles().iterator().next();
-    assertThat(profileConfiguration.getFromString("web"),
-        is(equalTo(new StringValueProfile("platform", "web"))));
     assertThat(config.getModuleName(), is(equalTo("myModule")));
+
+    Module module = config.getModule();
+    assertThat(module.getPageObjectsRootDirectory(), is(equalTo("/src/test/resources/spec")));
+    assertThat(module.getPageObjectsOutputDir(), is(equalTo("/src/test/java/pageObjects")));
+    assertThat(module.getResourcesOutputDir(), is(equalTo("/src/test/resources")));
+    assertThat(module.getUnitTestsOutputDir(), is(equalTo("/src/test/java")));
+    assertThat(module.getUnitTestRunnerType(), is(equalTo(UnitTestRunner.JUNIT)));
+    assertThat(module.getPageObjectFileMaskRegex(), is(equalTo("(.*)\\.utam\\.json$")));
+    assertThat(module.getName(), is(equalTo("myModule")));
+
+    Map<String,String> namespaces = module.getPackagesMapping();
+    assertThat(namespaces.keySet(), hasSize(2));
+    Namespace namespace = module.namespaces.get(0);
+    assertThat(namespace.getTypeMatch(), is(equalTo("utam-one")));
+    assertThat(namespaces.get("utam-one"), is(equalTo(".*/one")));
+    assertThat(namespace.getPackageMappingKey(), is(equalTo(namespace.getTypeMatch())));
+    assertThat(namespace.getPathMatch(), is(equalTo(".*/one")));
+    assertThat(namespace.getPackageMappingValue(), is(equalTo(namespace.getPathMatch())));
+
+    List<Profile> profiles = module.getRawProfiles();
+    assertThat(profiles, hasSize(1));
+    ProfileConfiguration profile = profiles.get(0).getProfileConfiguration();
+    assertThat(profile.getPropertyKey(), is(equalTo("platform")));
+    assertThat(profile.getSupportedValues(), containsInAnyOrder("ios", "android"));
   }
 
   @Test
-  public void testScanner() throws IOException {
+  public void testTargetConfigFromJsonFile() throws IOException {
+    JsonCompilerConfig config = getTestConfig();
+    Module module = config.getModule();
+
+    TranslatorTargetConfig targetConfig = module.getTargetConfig("path");
+    assertThat(targetConfig.getUnitTestRunnerType(), is(equalTo(UnitTestRunner.JUNIT)));
+    assertThat(targetConfig.getInjectionConfigRootFilePath(), is(equalTo("path/src/test/resources")));
+  }
+
+  @Test
+  public void testSourceConfigFromJsonFileScanner() throws IOException {
     JsonCompilerConfig config = getTestConfig();
     TranslatorSourceConfig sourceConfig = config.getSourceConfig();
     sourceConfig.recursiveScan();
@@ -77,39 +105,25 @@ public class JsonCompilerConfigTests {
   }
 
   @Test
-  public void testDefaultTargetValues() {
-    Target target = new Target("src", "res");
-    assertThat(target.pageObjectsPath, is(equalTo("src")));
-    assertThat(target.resourcesHomePath, is(equalTo("res")));
-    assertThat(target.unitTestRunnerType, is(equalTo(UnitTestRunner.NONE)));
-    assertThat(target.unitTestDirectory, is(emptyString()));
-  }
-
-  @Test
   public void testDefaultModuleProperties() {
     Module module = new Module("name", "pageObjectsDirectory");
     assertThat(module.getPageObjectFileMaskRegex(), is(equalTo(DEFAULT_JSON_FILE_MASK_REGEX)));
     assertThat(module.getName(), is(equalTo("name")));
-    assertThat(module.getPageObjectsDirectory(), is(equalTo("pageObjectsDirectory")));
-    assertThat(module.target, is(nullValue()));
+    assertThat(module.getPageObjectsRootDirectory(), is(equalTo("pageObjectsDirectory")));
     assertThat(module.getConfiguredProfiles(), is(emptyIterable()));
     assertThat(module.getPackagesMapping(), is(anEmptyMap()));
     module.getSourceConfig("");
-    UtamCompilationError e = expectThrows(UtamCompilationError.class,
-        () -> module.getTargetConfig(""));
-    assertThat(e.getMessage(), is(equalTo(ERR_TARGET_NOT_SET)));
+    module.getTargetConfig("");
   }
 
   @Test
   public void testNonDefaultModuleProperties() {
     Module module = new Module("name",
         "fileMask",
-        "pageObjectsDirectory",
-        new Target("pageObjects", "resources"));
+        "pageObjectsDirectory");
     module.namespaces.add(new Namespace("utam-package", "*/package"));
     module.getRawProfiles().add(new Profile("name", new String[]{"values"}));
     assertThat(module.getPageObjectFileMaskRegex(), is(equalTo("fileMask")));
-    assertThat(module.target, is(not(nullValue())));
     assertThat(module.getConfiguredProfiles(),
         hasItems(new StringValueProfileConfig("name", "values")));
     assertThat(module.getPackagesMapping().keySet(), hasItems("utam-package"));
@@ -171,38 +185,5 @@ public class JsonCompilerConfigTests {
         .readValue(PROFILE_JSON, Profile.class).getProfileConfiguration();
     assertThat(profileConfiguration,
         is(equalTo(new StringValueProfileConfig(PROFILE_NAME, new String[]{PROFILE_VALUE_1, PROFILE_VALUE_2}))));
-  }
-
-  @Test
-  public void testModuleFromJson() throws IOException {
-    final String MODULE_NAME = "testModule";
-    final String MODULE_PAGE_OBJECTS_DIRECTORY = "page/objects/directory";
-    final String TYPE_PREFIX = "utam-one";
-    final String TYPE_FOLDER_REGEX = "*/one";
-
-    final String MODULE_JSON = String.format("{\n"
-            + "  \"name\": \"%s\",\n"
-            + "  \"pageObjectsDirectory\": \"%s\",\n"
-            + "  \"namespaces\": [\n"
-            + "    {"
-            + "      \"typePrefix\": \"%s\",\n"
-            + "      \"pathMatchRegex\": \"%s\"\n"
-            + "    }\n"
-            + "  ]\n"
-            + "}",
-        MODULE_NAME,
-        MODULE_PAGE_OBJECTS_DIRECTORY,
-        TYPE_PREFIX,
-        TYPE_FOLDER_REGEX);
-
-    Module module = new ObjectMapper().readValue(MODULE_JSON, Module.class);
-    assertThat(module.getName(), is(equalTo(MODULE_NAME)));
-    assertThat(module.getPageObjectsDirectory(), is(equalTo(MODULE_PAGE_OBJECTS_DIRECTORY)));
-    assertThat(module.namespaces, hasSize(1));
-    Namespace namespace = module.namespaces.get(0);
-    assertThat(namespace.getTypePrefix(), is(equalTo(TYPE_PREFIX)));
-    assertThat(namespace.getPackageMappingKey(), is(equalTo(TYPE_PREFIX)));
-    assertThat(namespace.getPathMatchRegex(), is(equalTo(TYPE_FOLDER_REGEX)));
-    assertThat(namespace.getPackageMappingValue(), is(equalTo(TYPE_FOLDER_REGEX)));
   }
 }
--- utam-compiler/src/test/java/utam/compiler/translator/TranslationTypesConfigJavaTests.java
+++ utam-compiler/src/test/java/utam/compiler/translator/TranslationTypesConfigJavaTests.java
@@ -12,8 +12,6 @@ import utam.compiler.helpers.TranslationContext;
 import utam.core.framework.consumer.UtamError;
 import org.testng.annotations.Test;
 
-import static utam.compiler.translator.TranslationTypesConfigJava.Mask.pageObjects;
-import static utam.compiler.translator.TranslationTypesConfigJava.Mask.utils;
 import static utam.compiler.translator.TranslationTypesConfigJava.getWrongTypeError;
 import static utam.compiler.translator.TranslatorMockUtilities.TEST_URI;
 import static utam.compiler.translator.TranslatorMockUtilities.getDefaultConfig;
@@ -32,71 +30,47 @@ public class TranslationTypesConfigJavaTests {
 
   /** The getInterfaceType method should return a valid interface type */
   @Test
-  public void testGetInterfaceType() {
+  public void testValidInterfaceTypes() {
     assertThat(
         TYPES.getInterfaceType("utam-test/pageObjects/test/testObject").getFullName(),
         is(equalTo("utam.test.pageobjects.test.TestObject")));
+
     assertThat(
         TYPES.getInterfaceType("utam-test/pageObjects/test/object/testObject").getFullName(),
         is(equalTo("utam.test.pageobjects.test.object.TestObject")));
-  }
-  /** The getInterfaceType method should return a valid interface type with an abbreviated prefix */
-  @Test
-  public void testGetInterfaceTypeWithAbbreviatedPrefix() {
+
     assertThat(
         TYPES.getInterfaceType("utam/pageObjects/test/testObject").getFullName(),
         is(equalTo("utam.pageobjects.test.TestObject")));
   }
 
-  /**
-   * The getInterfaceType method should throw the proper exception with an invalid interface type
-   */
   @Test
-  public void testGetInterfaceTypeWithInvalidTypeThrows() {
+  public void testShortNamesThrows() {
     final String shortPageObjectUri = "utam-test/pageObjects";
     UtamError e = expectThrows(UtamError.class, () -> TYPES.getInterfaceType(shortPageObjectUri));
-    assertThat(e.getMessage(), containsString(getWrongTypeError(shortPageObjectUri, pageObjects)));
-  }
+    assertThat(e.getMessage(), containsString(getWrongTypeError(shortPageObjectUri)));
 
-  /**
-   * The getInterfaceType method should throw the proper exception with an invalid interface type
-   */
-  @Test
-  public void testGetInterfaceTypeWithInvalidPrefixThrows() {
-    final String pageObjectURI = "matu/pageObjects/testObject";
-    UtamError e = expectThrows(UtamError.class, () -> TYPES.getInterfaceType(pageObjectURI));
-    assertThat(e.getMessage(), containsString(getWrongTypeError(pageObjectURI, pageObjects)));
-  }
-
-  /**
-   * The getInterfaceType method should throw the proper exception with an invalid interface type
-   */
-  @Test
-  public void testGetInterfaceTypeWithInvalidAbbreviatedPrefixThrows() {
-    final String pageObjectURI = "matu/pageObjects/test/testObject";
-    UtamError e = expectThrows(UtamError.class, () -> TYPES.getInterfaceType(pageObjectURI));
-    assertThat(e.getMessage(), containsString(getWrongTypeError(pageObjectURI, pageObjects)));
-  }
-
-  /** The getInterfaceType method should throw the proper exception with an incorrect mask type */
-  @Test
-  public void testGetInterfaceTypeWithIncorrectMaskThrows() {
-    final String URI = "utam-test/utils/test/testObject";
-    UtamError e = expectThrows(UtamError.class, () -> TYPES.getInterfaceType(URI));
-    assertThat(e.getMessage(), containsString(getWrongTypeError(URI, pageObjects)));
+    final String shortUtilityUri = "utam-test/utils";
+    e = expectThrows(UtamError.class, () -> TYPES.getUtilityType(shortUtilityUri));
+    assertThat(e.getMessage(), containsString(getWrongTypeError(shortUtilityUri)));
   }
 
   @Test
-  public void testGetUtilityType() {
-    final String URI = "utam-test/utils/test/testObject";
+  public void testGetInterfaceTypeWithNonUtamPrefix() {
+    String URI = "lwr/pageObjects/test/testObject";
+    assertThat(TYPES.getInterfaceType(URI).getFullName(), is(equalTo("lwr.pageobjects.test.TestObject")));
+    URI = "lwr/NotPageObjects/testObject";
+    assertThat(TYPES.getInterfaceType(URI).getFullName(), is(equalTo("lwr.notpageobjects.TestObject")));
+  }
+
+  @Test
+  public void testValidUtilityTypes() {
+    String URI = "utam-test/utils/test/testObject";
     assertThat(TYPES.getUtilityType(URI).getFullName(), is(equalTo("utam.test.utils.test.TestObject")));
-  }
-
-  @Test
-  public void testGetUtilityTypeError() {
-    final String URI = "utam-test/pageObjects/test/testObject";
-    UtamError e = expectThrows(UtamError.class, () -> TYPES.getUtilityType(URI));
-    assertThat(e.getMessage(), containsString(getWrongTypeError(URI, utils)));
+    URI = "test/shmutils/test/testObject";
+    assertThat(TYPES.getUtilityType(URI).getFullName(), is(equalTo("test.shmutils.test.TestObject")));
+    URI = "test/shmutils/testObject";
+    assertThat(TYPES.getUtilityType(URI).getFullName(), is(equalTo("test.shmutils.TestObject")));
   }
 
   @Test
@@ -105,7 +79,7 @@ public class TranslationTypesConfigJavaTests {
     final String INVALID_PAGE_OBJECT_TYPE = "InvalidType";
     UtamError e = expectThrows(UtamError.class, () -> context.getType(INVALID_PAGE_OBJECT_TYPE));
     assertThat(
-        e.getMessage(), containsString(getWrongTypeError(INVALID_PAGE_OBJECT_TYPE, pageObjects)));
+        e.getMessage(), containsString(getWrongTypeError(INVALID_PAGE_OBJECT_TYPE)));
   }
 
   @Test
