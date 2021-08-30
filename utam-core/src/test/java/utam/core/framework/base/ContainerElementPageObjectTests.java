831a14c487ad1864d5fa83f28bde766381f6ef68
Elizaveta Ivanova <elizaveta.ivanova@salesforce.com>
2021-08-29 17:06:37 -0700

    container PO fix: correctly scoped locator

--- utam-core/src/test/java/utam/core/framework/base/ContainerElementPageObjectTests.java
+++ utam-core/src/test/java/utam/core/framework/base/ContainerElementPageObjectTests.java
@@ -13,11 +13,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.mock;
 import static org.testng.Assert.expectThrows;
+import static utam.core.element.FindContext.Type.EXISTING;
 import static utam.core.framework.base.ContainerElementPageObject.ERR_UNSUPPORTED_METHOD;
 
 import org.testng.annotations.Test;
+import utam.core.MockUtilities.MockDriver;
+import utam.core.element.ElementLocation;
 import utam.core.framework.UtamCoreError;
 import utam.core.framework.consumer.ContainerElement;
+import utam.core.framework.element.ElementLocationChain;
 import utam.core.selenium.element.LocatorBy;
 
 /**
@@ -46,7 +50,8 @@ public class ContainerElementPageObjectTests {
     ContainerElementPageObject pageObject = new ContainerElementPageObject(
         mock(ContainerElement.class));
 
-    UtamCoreError e = expectThrows(UtamCoreError.class, () -> pageObject.containsElement(mock(LocatorBy.class)));
+    UtamCoreError e = expectThrows(UtamCoreError.class,
+        () -> pageObject.containsElement(mock(LocatorBy.class)));
     assertThat(e.getMessage(), is(equalTo(ERR_UNSUPPORTED_METHOD)));
 
     e = expectThrows(UtamCoreError.class,
@@ -67,5 +72,53 @@ public class ContainerElementPageObjectTests {
 
     e = expectThrows(UtamCoreError.class, () -> pageObject.waitFor(() -> true));
     assertThat(e.getMessage(), is(equalTo(ERR_UNSUPPORTED_METHOD)));
+  }
+
+  @Test
+  public void testCorrectContainer() {
+    PageObjectsFactory factory = new MockDriver().getFactory();
+    TestContainersPage page = new TestContainersPage(factory);
+    ContainerElementPageObject testMe = page
+        .getContainerContent(ContainerElementPageObject.class, false);
+    assertThat(testMe.getRootLocationChain().getLocatorChainString(),
+        is(equalTo("driver > By.cssSelector: root > By.cssSelector: :scope > *:first-child")));
+
+    LoadMe test = testMe.test(LoadMe.class, "inject");
+    assertThat(test.getRootLocation().getLocatorChainString(), is(equalTo(
+        "driver > By.cssSelector: root > By.cssSelector: :scope > *:first-child > By.cssSelector: inject")));
+  }
+
+  @Test
+  public void testCorrectContainerInsideShadow() {
+    PageObjectsFactory factory = new MockDriver().getFactory();
+    TestContainersPage page = new TestContainersPage(factory);
+    ContainerElementPageObject testMe = page
+        .getContainerContent(ContainerElementPageObject.class, true);
+    assertThat(testMe.getRootLocationChain().getLocatorChainString(),
+        is(equalTo("driver > By.cssSelector: root >> By.cssSelector: :scope > *:first-child")));
+
+    LoadMe test = testMe.test(LoadMe.class, "inject");
+    assertThat(test.getRootLocation().getLocatorChainString(), is(equalTo(
+        "driver > By.cssSelector: root >> By.cssSelector: :scope > *:first-child >> By.cssSelector: inject")));
+  }
+
+  public static class LoadMe extends BasePageObject {
+
+    ElementLocation getRootLocation() {
+      return super.root;
+    }
+  }
+
+  static class TestContainersPage extends BasePageObject {
+
+    TestContainersPage(PageObjectsFactory factory) {
+      factory.bootstrap(this, new ElementLocationChain(LocatorBy.byCss("root"), EXISTING));
+    }
+
+    final <T extends PageObject> T getContainerContent(Class<T> pageObjectType,
+        boolean isExpandRoot) {
+      return this.inContainer(this.root, isExpandRoot)
+          .load(pageObjectType, LocatorBy.byCss(":scope > *:first-child"));
+    }
   }
 }
