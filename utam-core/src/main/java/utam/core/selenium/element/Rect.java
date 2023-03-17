126b31d201ccb29b494b4cfd56644b3b37b90095
whcas <whcasa@gmail.com>
2023-03-17 15:31:16 +0000

    style(utam-core): Fix indentation style in Rect

--- utam-core/src/main/java/utam/core/selenium/element/Rect.java
+++ utam-core/src/main/java/utam/core/selenium/element/Rect.java
@@ -11,51 +11,51 @@ import org.openqa.selenium.Point;
  */
 public class Rect {
 
-    private int xPosition;
-    private int yPosition;
-    private int rectWidth;
-    private int rectHeight;
+  private int xPosition;
+  private int yPosition;
+  private int rectWidth;
+  private int rectHeight;
 
-    public Rect(Point windowPoint, Dimension windowSize) {
-        this.xPosition = windowPoint.getX();
-        this.yPosition = windowPoint.getY();
-        this.rectWidth = windowSize.getWidth();
-        this.rectHeight = windowSize.getHeight();
-    }
+  public Rect(Point windowPoint, Dimension windowSize) {
+    this.xPosition = windowPoint.getX();
+    this.yPosition = windowPoint.getY();
+    this.rectWidth = windowSize.getWidth();
+    this.rectHeight = windowSize.getHeight();
+  }
 
-    /**
-     * Gets the x position of this Rect
-     *
-     * @return the x coordinate of this Rect
-     */
-    public int getX() {
-        return this.xPosition;
-    }
+  /**
+   * Gets the x position of this Rect
+   *
+   * @return the x coordinate of this Rect
+   */
+  public int getX() {
+    return this.xPosition;
+  }
 
-    /**
-     * Gets the y position of this Rect
-     *
-     * @return the y coordinate of this Rect
-     */
-    public int getY() {
-        return this.yPosition;
-    }
+  /**
+   * Gets the y position of this Rect
+   *
+   * @return the y coordinate of this Rect
+   */
+  public int getY() {
+    return this.yPosition;
+  }
 
-    /**
-     * Gets the width position of this Rect
-     *
-     * @return the width coordinate of this Rect
-     */
-    public int getWidth() {
-        return this.rectWidth;
-    }
+  /**
+   * Gets the width position of this Rect
+   *
+   * @return the width coordinate of this Rect
+   */
+  public int getWidth() {
+    return this.rectWidth;
+  }
 
-    /**
-     * Gets the height position of this Rect
-     *
-     * @return the height coordinate of this Rect
-     */
-    public int getHeight() {
-        return this.rectHeight;
-    }
+  /**
+   * Gets the height position of this Rect
+   *
+   * @return the height coordinate of this Rect
+   */
+  public int getHeight() {
+    return this.rectHeight;
+  }
 }
